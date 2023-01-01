89ba80f29f4395e943bd45cce6686894ab512a3d
Mahijith <menon.mahijith@gmail.com>
2023-01-01 23:17:34 +0530

    added html classes

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const letterPiece=document.querySelectorAll('.board-letter');
-console.log(letterPiece);
+const player=document.querySelectorAll('.player');
+const p1=document.querySelector('.p-1');
+const p2=document.querySelector('.p-2');
+console.log(player);
+
+// p1.textContent=prompt("Enter Your Name");
+// p2.textContent=prompt("Enter Your Friend's Name");
+
+for(letter of letterPiece){
+letter.addEventListener('click',function(){
+    console.log("Clicked")
+})
+}
